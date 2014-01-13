0422a8c221650ddba9e43dff8702a3046b3d9c83
Mike McLin <mclinmike@gmail.com>
2014-01-12 23:38:19 -0500

    Initial commit

---
+++ src/MikeMcLin/WpPassword/WpPasswordServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace MikeMcLin\WpPassword;
+
+use Illuminate\Support\ServiceProvider;
+
+class WpPasswordServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('mikemclin/wp-password');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
