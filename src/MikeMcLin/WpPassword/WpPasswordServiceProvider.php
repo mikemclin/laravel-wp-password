f886fe1819990e40fe487201874e720043f975cf
Mike McLin <mclinmike@gmail.com>
2014-01-12 23:38:56 -0500

    Add `WpPassword` alias

--- src/MikeMcLin/WpPassword/WpPasswordServiceProvider.php
+++ src/MikeMcLin/WpPassword/WpPasswordServiceProvider.php
@@ -1,44 +1,47 @@
 <?php namespace MikeMcLin\WpPassword;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Foundation\AliasLoader;
 
 class WpPasswordServiceProvider extends ServiceProvider {
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->package('mikemclin/wp-password');
-	}
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->package('mikemclin/wp-password');
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		//
-	}
+        AliasLoader::getInstance()->alias('WpPassword', 'MikeMcLin\WpPassword\WpPassword');
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array();
-	}
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array();
+    }
 
 }
