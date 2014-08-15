5c541b6889d307823505749fc457347e3f5ff862
Mike McLin <mclinmike@gmail.com>
2014-08-14 23:51:41 -0400

    Add tests for `WpPassword::getInstance()`

--- tests/WpPasswordTest.php
+++ tests/WpPasswordTest.php
@@ -10,6 +10,33 @@ class WpPasswordTest extends PHPUnit_Framework_TestCase
         Mockery::close();
     }
 
+    public function test_makeInstance_returns_a_WpPassword_instance()
+    {
+        $response = WpPassword::getInstance();
+
+        $this->assertInstanceOf('MikeMcLin\WpPassword\WpPassword', $response);
+    }
+
+    public function test_makeInstance_returns_same_instance()
+    {
+        $response = WpPassword::getInstance();
+
+        $response2 = WpPassword::getInstance();
+
+        $this->assertEquals($response, $response2);
+    }
+
+    public function test_makeInstance_creates_new_instance_when_PasswordHash_is_passed()
+    {
+        $wp_hasher = Mockery::mock('Hautelook\Phpass\PasswordHash');
+
+        $response = WpPassword::getInstance();
+
+        $response2 = WpPassword::getInstance($wp_hasher);
+
+        $this->assertNotEquals($response, $response2);
+    }
+
     public function test_make_method_calls_HashPassword_and_returns_result()
     {
         $wp_hasher = Mockery::mock('Hautelook\Phpass\PasswordHash');
